06c6abef107b9567a7bb79f4d8f9ca0efa779423
Kateryna <104839199+Kate1985sky@users.noreply.github.com>
2022-10-20 16:43:34 +0300

    project without css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./Style.css";
 
 function App() {
+  const [temperature, setTemperature] = useState("");
+  const [condition, setCondition] = useState("");
+  const [inputCity, setInputCity] = useState("Kiev");
+  const [error, setError] = useState(false);
+
+  const updateWether = () => {
+    console.log("update wheather");
+    console.log(inputCity);
+ 
+  fetch(
+    "https://api.weatherapi.com/v1/current.json?key=e9709cf4f7b74389b1f133756221910&q="+inputCity+"&aqi=no"
+  )
+    .then((el) => el.json())
+    .then((data) => {
+      setTemperature(data.current.temp_c);
+      setCondition(data.current.condition.text);
+    })
+    .catch(error => setError(true));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {error && <div>Міста немає</div>}
+      {
+      temperature && condition && 
+        <>
+          <header className="App-header">
+            <h1>Прогноз погоди в {inputCity}</h1>
+          </header>
+          <div className="wrapper">
+            <ul className="list">
+              <li>
+                Температура повітря: {temperature} <br />
+              </li>
+              <li>Опади: {condition}</li>
+            </ul>
+          </div>
+        </>
+}
+      <div>
+        <input
+          type="text"
+          value={inputCity}
+          onChange={(event) => setInputCity(event.target.value)}
+        />
+        <button type="button" onClick={updateWether}>
+          Показати погоду в місті
+        </button>
+      </div>
     </div>
   );
 }
